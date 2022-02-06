7fe6eb3479e37a26f098d0fcc6173a0768d51e7b
Arnaud Vrac <rawoul@gmail.com>
2022-02-07 00:33:50 +0100

    fish: add config files

---
+++ .config/fish/config.fish
@@ -0,0 +1,74 @@
+# features
+set -U fish_features 3.0 3.1
+set -g fish_greeting
+
+# ensure perms
+umask 022
+
+# env
+fish_add_path ~/bin
+set -gx BROWSER firefox
+set -gx EDITOR vim
+set -gx LESS "-q -R -S"
+set -gx LESS_TERMCAP_mb \e\[01\;34m
+set -gx LESS_TERMCAP_md \e\[01\;34m
+set -gx LESS_TERMCAP_me \e\[0m
+set -gx LESS_TERMCAP_se \e\[0m
+set -gx LESS_TERMCAP_so \e\[01\;44\;37m
+set -gx LESS_TERMCAP_ue \e\[0m
+set -gx LESS_TERMCAP_us \e\[00\;36m
+set -gx PAGER less
+
+# aliases
+if status --is-interactive
+    alias du "du -h"
+    alias df "df -h"
+    alias cp "cp -i"
+    alias mv "mv -i"
+    alias l "ls -1"
+    function ddg
+        $BROWSER "https://duckduckgo.com?k1=-1&kp=-2&kd=-1&q="(string escape --style=url "$argv")
+    end
+    if [ -f /etc/debian_version ]
+        function apts; apt-cache search "$argv" | grep -i "$argv"; end
+        function aptr; apt remove --purge $argv; end
+        function apti; apt install --no-install-recommends $argv; end
+    end
+end
+
+# colors
+if status --is-login
+    set -U fish_color_autosuggestion 2e3440
+    set -U fish_color_cancel -r
+    set -U fish_color_command brcyan
+    set -U fish_color_comment 434c5e
+    set -U fish_color_cwd blue
+    set -U fish_color_cwd_root red
+    set -U fish_color_end brgreen
+    set -U fish_color_error brred
+    set -U fish_color_escape 00a6b2
+    set -U fish_color_history_current --bold
+    set -U fish_color_host brwhite
+    set -U fish_color_match --background=brblue
+    set -U fish_color_normal normal
+    set -U fish_color_operator green
+    set -U fish_color_param brwhite
+    set -U fish_color_quote bryellow
+    set -U fish_color_redirection brmagenta
+    set -U fish_color_search_match --background=203e60
+    set -U fish_color_selection 'white'  '--bold'  '--background=brblack'
+    set -U fish_color_user blue
+    set -U fish_color_valid_path brwhite
+    set -U fish_pager_color_completion normal
+    set -U fish_pager_color_description 'B3A06D'  'yellow'
+    set -U fish_pager_color_prefix 'normal'  '--bold'  '--underline'
+    set -U fish_pager_color_progress 'brblack'  '--background=222222'
+end
+
+# fzf
+if [ -x ~/.vim/bundle/fzf/bin/fzf ]
+    fish_add_path ~/.vim/bundle/fzf/bin
+    source ~/.vim/bundle/fzf/shell/key-bindings.fish
+    set -x FZF_DEFAULT_OPTS --reverse
+    fzf_key_bindings
+end
---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,18 @@
+function fish_prompt
+    set -l hi_color blue
+    set -l text_color brwhite
+    if [ $USER = root ]
+        set text_color red
+    end
+
+    set -l host_color $text_color
+    if test -n "$SSH_TTY"
+        set host_color yellow
+    end
+
+    set_color --bold
+    if [ $USER != rawoul ]
+        echo -n (set_color $text_color)"$USER"(set_color $hi_color)'@'
+    end
+    echo -n (set_color $host_color)(prompt_hostname)(set_color $hi_color)' ❯ '(set_color $text_color)(prompt_pwd)(set_color $hi_color)' ❯❯ '
+end
